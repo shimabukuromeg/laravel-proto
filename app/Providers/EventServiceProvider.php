f812d9544a6c1b744d3d49cb983ba2306b109c7a
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-09-02 22:28:51 +0900

    イベント／リスナ生成

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,7 +18,11 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'App\Events\PersonEvent' => [
+            'App\Listeners\PersonEventListener',
+        ],
     ];
+
 
     /**
      * Register any events for your application.
