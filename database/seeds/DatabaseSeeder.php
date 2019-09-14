88c3fe377038bd396aa11991be7c3030e0530308
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-09-14 12:07:48 +0900

    [update] シーダーの登録

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(AuthorsTableSeeder::class);
         $this->call(BookdetailsTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
