4fb8d7c5f1d0e4eb6a9c7da6f78892d790952a3e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-09-14 10:39:08 +0900

    [update] 5-1 マイグレーション章のサンプルコード作成

---
+++ database/migrations/2019_09_14_013007_create_publishers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublishersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publishers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', '100');
+            $table->string('bookdetails_id');
+            $table->integer('author_id');
+            $table->integer('publisher_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('publishers');
+    }
+}
