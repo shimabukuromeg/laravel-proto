7ec506e843cc9dde4db3fbad839189bf4c2b8ef3
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-09-15 14:41:55 +0900

    [update] Seviceクラスはインターフェースを参照するようにするため、修正

--- app/Services/FavoriteService.php
+++ app/Services/FavoriteService.php
@@ -3,30 +3,19 @@ declare(strict_types=1);
 
 namespace App\Services;
 
-use App\DataProvider\Eloquent\Favorite;
+use App\DataProvider\FavoriteRepositoryInterface;
 
 class FavoriteService
 {
+    private $favorite;
+
+    public function __construct(FavoriteRepositoryInterface $favorite)
+    {
+        $this->favorite = $favorite;
+    }
+
     public function switchFavorite(int $bookId, int $userId, string $createdAt): int
     {
-        return \DB::transaction(
-            function() use ($bookId, $userId, $createdAt) {
-                $count = Favorite::where('book_id', $bookId)
-                    ->where('user_id', $userId)
-                    ->count();
-                if ($count == 0) {
-                    Favorite::create([
-                        'book_id' => $bookId,
-                        'user_id' => $userId,
-                        'created_ad' => $createdAt
-                    ]);
-                    return 1;
-                }
-                Favorite::where('book_id', $bookId)
-                    ->where('user_id', $userId)
-                    ->delete();
-                return 0;
-            }
-        );
+        return $this->favorite->switch($bookId, $userId, $createdAt);
     }
 }
