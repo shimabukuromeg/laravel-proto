0de18b7668fc24be93c116ce1a801bb18ea9d168
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-09-21 20:18:25 +0900

    [update] snsログインのリダイレクト先とコールバックの処理を追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Socialite;
+use App\User;
+use Auth;
+
 class LoginController extends Controller
 {
     /*
@@ -35,5 +39,34 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * @param $social
+     * @return mixed
+     */
+    public function socialLogin($social)
+    {
+        return Socialite::driver($social)->redirect();
+    }
+
+    public function handleProviderCallback($social)
+    {
+        $userSocial = Socialite::driver($social)->stateless()->user();
+
+        $user = User::where(['email' => $userSocial->getEmail()])->first();
+
+        if ($user) {
+            Auth::login($user);
+            return redirect('/home');
+        } else {
+            $newUser = new User;
+            $newUser->name = $userSocial->getName();
+            $newUser->email = $userSocial->getEmail();
+            $newUser->save();
+
+            Auth::login($newUser);
+            return redirect('/home');
+        }
     }
 }
