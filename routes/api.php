2ec66b3b7fc389f6279f441e4635d197c409d1f7
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-10-05 17:23:29 +0900

    [update] 全てのエンドポイントに対してテストを作成

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('customers', function() {});
+Route::post('customers', function() {});
+Route::get('customers/{customer_id}', function() {});
+Route::put('customers/{customer_id}', function() {});
+Route::delete('customers/{customer_id}', function() {});
+Route::get('reports', function() {});
+Route::post('reports', function() {});
+Route::get('reports/{report_id}', function() {});
+Route::put('reports/{report_id}', function() {});
+Route::delete('reports/{report_id}', function() {});
+
--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -16,4 +16,83 @@ class ReportTest extends TestCase
         $response = $this->get('api/customers');
         $response->assertStatus(200);
     }
+
+    public function api_customersにPOSTメソッドでアクセスできる()
+    {
+        $response = $this->post('api/customers');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにPUTメソッドでアクセスできる()
+    {
+        $response = $this->put('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_customers_customer_idにDELETEメソッドでアクセスできる()
+    {
+        $response = $this->delete('api/customers/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reportsにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reportsにPOSTメソッドでアクセスできる()
+    {
+        $response = $this->post('api/reports');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにGETメソッドでアクセスできる()
+    {
+        $response = $this->get('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにPUTメソッドでアクセスできる()
+    {
+        $response = $this->put('api/reports/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function api_reports_report_idにDELETEメソッドでアクセスできる()
+    {
+        $response = $this->delete('api/reports/1');
+        $response->assertStatus(200);
+    }
+
 }
